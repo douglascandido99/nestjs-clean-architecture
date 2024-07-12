54426babe406ce6969464b99ecc9fdf6698afab3
Douglas Candido <douglascandido@workmail.com>
2024-07-12 12:23:55 -0300

    applySort method unit tests

--- src/shared/domain/repositories/__tests__/unit/searchable-in-memory-repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/searchable-in-memory-repository.spec.ts
@@ -60,7 +60,34 @@ describe('InMemoryRepository unit tests', () => {
     })
   })
 
-  describe('applySort method', () => {})
+  describe('applySort method', () => {
+    it('should not sort items', async () => {
+      const items = [
+        new StubEntity({ name: 'b', price: 50 }),
+        new StubEntity({ name: 'a', price: 50 }),
+      ]
+
+      let sortedItems = await sut['applySort'](items, null, null)
+      expect(sortedItems).toStrictEqual(items)
+
+      sortedItems = await sut['applySort'](items, 'price', 'asc')
+      expect(sortedItems).toStrictEqual(items)
+    })
+
+    it('should sort items', async () => {
+      const items = [
+        new StubEntity({ name: 'b', price: 50 }),
+        new StubEntity({ name: 'a', price: 50 }),
+        new StubEntity({ name: 'c', price: 50 }),
+      ]
+
+      let sortedItems = await sut['applySort'](items, 'name', 'asc')
+      expect(sortedItems).toStrictEqual([items[1], items[0], items[2]])
+
+      sortedItems = await sut['applySort'](items, 'name', 'desc')
+      expect(sortedItems).toStrictEqual([items[2], items[0], items[1]])
+    })
+  })
 
   describe('applyPaginate method', () => {})
 
